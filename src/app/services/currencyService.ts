8d0137f3ec49adbd5d8d42a80c7b9d9d3cbcfcc8
Pedro-melo12 <pedrinhomelo2005@gmail.com>
2024-11-25 13:26:40 -0300

    Alteração no serviço para funcionamento offline

--- src/app/services/currencyService.ts
+++ src/app/services/currencyService.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Storage } from '@ionic/storage-angular';
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -7,9 +10,33 @@ import { HttpClient } from '@angular/common/http';
 export class CurrencyService {
   private apiUrl = 'https://api.exchangerate-api.com/v4/latest/';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private storage: Storage) {
+    this.initStorage();
+  }
 
-  getExchangeRates(baseCurrency: string) {
-    return this.http.get(`${this.apiUrl}${baseCurrency}`);
+  async initStorage() {
+    await this.storage.create();
+  }
+
+  
+  getExchangeRates(baseCurrency: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}${baseCurrency}`).pipe(
+    
+      tap(async (data: any) => {
+        await this.storage.set('rates', {
+          date: new Date().toISOString(),
+          rates: data,
+        });
+      }),
+      
+      catchError(async () => {
+        const savedRates = await this.storage.get('rates');
+        if (savedRates) {
+          return of(savedRates.rates); 
+        } else {
+          throw new Error('Sem conexão e sem dados armazenados.');
+        }
+      })
+    );
   }
 }
