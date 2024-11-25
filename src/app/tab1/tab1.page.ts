3e0e2c88defbd6ff808e34081fca95a9c5e2e584
Pedro-melo12 <pedrinhomelo2005@gmail.com>
2024-11-25 13:05:20 -0300

    Projeto conversor de moedas

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  templateUrl: './tab1.page.html',
+  styleUrls: ['./tab1.page.scss'],
 })
 export class Tab1Page {
+  email: string = '';
+  password: string = '';
 
-  constructor() {}
+  constructor(private alertController: AlertController) {}
 
+  async login() {
+    if (this.email === '' || this.password === '') {
+      const alert = await this.alertController.create({
+        header: 'Erro',
+        message: 'Por favor, preencha todos os campos.',
+        buttons: ['OK'],
+      });
+      await alert.present();
+      return;
+    }
+
+    // Lógica de autenticação (a ser conectada com seu backend ou Firebase)
+    if (this.email === 'teste@teste.com' && this.password === '123456') {
+      const alert = await this.alertController.create({
+        header: 'Sucesso',
+        message: 'Login realizado com sucesso!',
+        buttons: ['OK'],
+      });
+      await alert.present();
+    } else {
+      const alert = await this.alertController.create({
+        header: 'Erro',
+        message: 'Credenciais inválidas.',
+        buttons: ['OK'],
+      });
+      await alert.present();
+    }
+  }
+
+  register() {
+    console.log('Redirecionar para tela de registro');
+    // Redirecione para a tela de registro
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CurrencyService } from '../services/currencyService';
 
 @Component({
   selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  templateUrl: './tab2.page.html',
+  styleUrls: ['./tab2.page.scss'],
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  currencies: string[] = [];
+  baseCurrency = 'USD';
+  targetCurrency = 'EUR';
+  amount = 1;
+  conversionResult: number | null = null;
 
-  constructor() {}
+  constructor(private currencyService: CurrencyService) {}
 
+  ngOnInit() {
+    this.loadCurrencies();
+  }
+
+  loadCurrencies() {
+    this.currencyService.getExchangeRates('USD').subscribe((data: any) => {
+      this.currencies = Object.keys(data.rates);
+    });
+  }
+
+  convert() {
+    this.currencyService.getExchangeRates(this.baseCurrency).subscribe((data: any) => {
+      const rate = data.rates[this.targetCurrency];
+      this.conversionResult = this.amount * rate;
+    });
+  }
 }
